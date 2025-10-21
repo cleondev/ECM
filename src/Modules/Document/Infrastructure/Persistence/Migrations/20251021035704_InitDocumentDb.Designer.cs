505472d78c1e8484d0820cd11bc21f075d6a4375
SonLH <sonlh@ists.com.vn>
2025-10-21 10:57:36 +0700

    #1 add document init migration

--- src/Modules/Document/Infrastructure/Persistence/Migrations/20251021035704_InitDocumentDb.Designer.cs
+++ src/Modules/Document/Infrastructure/Persistence/Migrations/20251021035704_InitDocumentDb.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using ECM.Document.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace ECM.Document.Infrastructure.Migrations
+namespace ECM.Document.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(DocumentDbContext))]
-    partial class DocumentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251021035704_InitDocumentDb")]
+    partial class InitDocumentDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
