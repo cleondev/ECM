50fdbbdb13f5c3c6d1868be0ebf08c28ba655422
SonLH <sonlh@ists.com.vn>
2025-10-21 10:43:24 +0700

    1

--- src/Modules/Abstractions/Persistence/MigrationBuilderExtensions.cs
+++ src/Modules/Abstractions/Persistence/MigrationBuilderExtensions.cs
@@ -9,8 +9,6 @@ public static class MigrationBuilderExtensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS btree_gin;
-CREATE EXTENSION IF NOT EXISTS vector;
 """;
 
     public static void EnsurePostgresExtensions(this MigrationBuilder migrationBuilder)
