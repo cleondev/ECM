c1d5e9c7236d3271fa4f0141cbf324392aae7bee
cleondev <sudosonlh@gmail.com>
2025-10-21 16:33:13 +0700

    Prevent login redirects on API authorization failures

--- src/AppGateway/AppGateway.Api/Controllers/WorkflowsController.cs
+++ src/AppGateway/AppGateway.Api/Controllers/WorkflowsController.cs
@@ -1,4 +1,5 @@
 using AppGateway.Contracts.Workflows;
+using AppGateway.Api.Auth;
 using AppGateway.Infrastructure.Ecm;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -8,7 +9,7 @@ namespace AppGateway.Api.Controllers;
 
 [ApiController]
 [Route("api/workflows")]
-[Authorize]
+[Authorize(AuthenticationSchemes = GatewayAuthenticationSchemes.Default)]
 public sealed class WorkflowsController(IEcmApiClient client) : ControllerBase
 {
     private readonly IEcmApiClient _client = client;
