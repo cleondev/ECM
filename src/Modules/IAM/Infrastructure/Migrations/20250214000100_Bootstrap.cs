9f5694c305d6ef0941f10dd22f7e66e26e51c9fa
cleondev <sudosonlh@gmail.com>
2025-10-21 10:36:03 +0700

    Add bootstrap migrations to ensure PostgreSQL extensions

---
+++ src/Modules/IAM/Infrastructure/Migrations/20250214000100_Bootstrap.cs
@@ -0,0 +1,18 @@
+using ECM.Modules.Abstractions.Persistence;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECM.IAM.Infrastructure.Migrations;
+
+public partial class Bootstrap : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.EnsurePostgresExtensions();
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
