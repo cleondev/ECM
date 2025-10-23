561320ce0dedb929fd2cd5fe862296f792605532
cleondev <sudosonlh@gmail.com>
2025-10-23 14:01:17 +0700

    Handle Microsoft Identity Web challenge when acquiring user tokens

--- src/AppGateway/AppGateway.Infrastructure/Ecm/EcmApiClient.cs
+++ src/AppGateway/AppGateway.Infrastructure/Ecm/EcmApiClient.cs
@@ -460,7 +460,7 @@ internal sealed class EcmApiClient(
                 request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
                 return;
             }
-            catch (MsalException exception)
+            catch (Exception exception) when (exception is MsalException or MicrosoftIdentityWebException)
             {
                 _logger.LogWarning(
                     exception,
