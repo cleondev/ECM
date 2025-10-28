527df2a39a4dc6b1a8e0b69e4cf2b21ce2bb8889
cleondev <sudosonlh@gmail.com>
2025-10-28 17:45:31 +0700

    Increase document upload size limits

--- src/Modules/Document/Api/DocumentModule.cs
+++ src/Modules/Document/Api/DocumentModule.cs
@@ -1,11 +1,15 @@
+using System;
 using ECM.Abstractions;
 using ECM.Document.Api.Documents;
 using ECM.Document.Api.Tags;
 using ECM.Document.Application;
 using ECM.Document.Infrastructure;
 
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace ECM.Document.Api;
@@ -18,6 +22,36 @@ public sealed class DocumentModule : IModule
         services.AddDocumentInfrastructure();
         services.AddOptions<DocumentUploadDefaultsOptions>()
             .BindConfiguration(DocumentUploadDefaultsOptions.SectionName);
+
+        services.AddOptions<DocumentUploadLimitOptions>()
+            .BindConfiguration(DocumentUploadLimitOptions.SectionName)
+            .Validate(static options =>
+            {
+                try
+                {
+                    options.EnsureValid();
+                    return true;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    return false;
+                }
+            }, "Document upload limits must be greater than zero.")
+            .ValidateOnStart();
+
+        services.AddOptions<FormOptions>()
+            .Configure<DocumentUploadLimitOptions>((options, uploadLimits) =>
+            {
+                options.MultipartBodyLengthLimit = uploadLimits.MultipartBodyLengthLimit;
+                options.ValueLengthLimit = int.MaxValue;
+                options.MemoryBufferThreshold = int.MaxValue;
+            });
+
+        services.AddOptions<KestrelServerOptions>()
+            .Configure<DocumentUploadLimitOptions>((options, uploadLimits) =>
+            {
+                options.Limits.MaxRequestBodySize = uploadLimits.MaxRequestBodySize;
+            });
         services.ConfigureModuleSwagger(DocumentSwagger.DocumentName, DocumentSwagger.Info);
     }
 
