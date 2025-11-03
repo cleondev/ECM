1332ea55b2095b04d8aebbe3cce76e644c2da5da
SonLH <sonlh@ists.com.vn>
2025-11-03 10:40:45 +0700

    1

--- src/AppGateway/ui/components/upload-dialog.tsx
+++ src/AppGateway/ui/components/upload-dialog.tsx
@@ -733,7 +733,7 @@ export function UploadDialog({ open, onOpenChange, onUploadComplete }: UploadDia
               <Dashboard
                 uppy={uppy}
                 width="100%"
-                height={340}
+                height={300}
                 hideUploadButton
                 proudlyDisplayPoweredByUppy={false}
                 showProgressDetails
