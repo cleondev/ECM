2a4ea160fe1b1f31fa9a049c46a42805317d8753
cleondev <sudosonlh@gmail.com>
2025-11-25 14:12:07 +0700

    Handle viewer params client-side for static export

--- src/AppGateway/ui/app/viewer/page.tsx
+++ src/AppGateway/ui/app/viewer/page.tsx
@@ -1,8 +1,5 @@
 import { ViewerPageClient } from "./viewer-page-client"
 
-export const dynamic = "force-static"
-export const dynamicParams = false
-
 export default function FileViewPage() {
   return <ViewerPageClient />
 }
