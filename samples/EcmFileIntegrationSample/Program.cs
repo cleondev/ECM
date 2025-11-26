8828cce62f35ca50e9365625e68156444d7e88a9
SonLH <sonlh@ists.com.vn>
2025-11-26 10:54:55 +0700

    1

--- samples/EcmFileIntegrationSample/Program.cs
+++ samples/EcmFileIntegrationSample/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 using Ecm.Sdk;
+using samples.EcmFileIntegrationSample;
 
 var builder = WebApplication.CreateBuilder(args);
 
